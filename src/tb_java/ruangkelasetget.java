1d9b0fef97e797bf0cedb934578ccc9fcf932cb3
bayufajar <fajarkecoak@gmail.com>
2015-04-20 20:45:58 +0700

    new commint

--- src/tb_java/ruangkelasetget.java
+++ src/tb_java/ruangkelasetget.java
@@ -10,5 +10,53 @@ package tb_java;
  * @author espadafajar
  */
 public class ruangkelasetget {
-    
+String namaruang;
+String lokasiruang;
+String faJur;
+int panjang;
+int lebar;
+int luas;
+int jumlah_kursi;
+int jumlah_pintu;
+int jumlahjendela;
+int rasioluas;
+String kondisiruang;
+String kondpintu;
+String kondjendela;
+String konddinding;
+String kondatap;
+String kondlantai;
+String sirkudara;
+int pencahayaan;
+int kelembapan;
+int celcius;
+String kebisingan;
+String bau;
+String kebocoran;
+String kerusakan;
+String keausan;
+String kekohan;
+String KPJ;
+String bahaya;
+int jumkontak;
+String kondkontak;
+String poskontak;
+int kabelLCD;
+String kondkabLCD;
+String poskabLCD;
+int jumLampu;
+String kondlampu;
+String poslampu;
+int jumKipas;
+String kondkipas;
+String posKipas;
+int jumAC;
+String kondAC;
+String posAC;
+String SSID;
+int bandwith;
+int jumCCTV;
+String kondCTV;
+String posCTV;
+
 }
