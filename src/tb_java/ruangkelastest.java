a2cfce6eb7b52b4c7eddeb4588ef45fbcaa9e881
bayufajar <fajarkecoak@gmail.com>
2015-04-22 16:25:34 +0700

    new commint

--- src/tb_java/ruangkelastest.java
+++ src/tb_java/ruangkelastest.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tb_java;
 import java.util.*;
-/**
- *
- * @author espadafajar
- */
+
+
 public class ruangkelastest {
     public static void main(String[] args)
     {
